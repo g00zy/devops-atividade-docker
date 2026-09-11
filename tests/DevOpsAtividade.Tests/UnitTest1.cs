1b01eab17c9b99f74fe86a30cb446b53c1303d8f
Gabriel Mansano <gabrielmansano@gmail.com>
2026-09-10 23:00:48 -0300

    test: adiciona teste unitario inicial

--- tests/DevOpsAtividade.Tests/UnitTest1.cs
+++ tests/DevOpsAtividade.Tests/UnitTest1.cs
@@ -3,8 +3,13 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Soma_DeveRetornarResultadoCorreto()
     {
+        int numero1 = 2;
+        int numero2 = 3;
 
+        int resultado = numero1 + numero2;
+
+        Assert.Equal(5, resultado);
     }
 }
